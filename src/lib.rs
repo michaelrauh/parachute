cb088b7c08e07ea8733530eda116bbee56b0395f
michaelrauh <michaelrauh@ymail.com>
2024-04-07 15:26:48 -0400

    run formatter, make add resilient to bucket existing or not existing. Lots of round trips. Still only one e2e which doesnt entirely cover

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,26 @@ use aws_sdk_s3::primitives::ByteStream;
 
 #[::tokio::main]
 pub async fn add(file_name: String, endpoint: String, location: String) {
-    let config = aws_config::from_env()
-    .endpoint_url(endpoint)
-    .load()
-    .await;
+    let config = aws_config::from_env().endpoint_url(endpoint).load().await;
     let client = aws_sdk_s3::Client::new(&config);
+
+    if !client
+        .list_buckets()
+        .send()
+        .await
+        .unwrap()
+        .buckets()
+        .to_vec()
+        .iter()
+        .any(|b| b.name().unwrap_or_default() == location)
+    {
+        client
+            .create_bucket()
+            .bucket(&location)
+            .send()
+            .await
+            .unwrap();
+    }
 
     let body = ByteStream::from_path(Path::new(&file_name)).await;
     client
@@ -17,5 +32,6 @@ pub async fn add(file_name: String, endpoint: String, location: String) {
         .key(file_name)
         .body(body.unwrap())
         .send()
-        .await.unwrap();
+        .await
+        .unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,23 @@ use parachute::add;
 #[derive(Parser, Debug)]
 #[command(about, long_about = None)]
 struct Args {
-    #[arg(short, long, value_name = "S3 PATH",)]
+    #[arg(short, long, value_name = "S3 PATH")]
     location: String,
 
     #[arg(short, long)]
     endpoint: String,
 
-    #[arg(short, long, value_name = "FILENAME",)]
+    #[arg(short, long, value_name = "FILENAME")]
     add: Option<String>,
 
-    #[arg(short, long, conflicts_with = "add",)]
+    #[arg(short, long, conflicts_with = "add")]
     mode: Option<AgentMode>,
 }
 
 #[derive(Parser, Debug, Clone, ValueEnum)]
 enum AgentMode {
-    Single, Merge
+    Single,
+    Merge,
 }
 
 fn main() {
