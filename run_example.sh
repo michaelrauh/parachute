548bbda76fb4f3b1b8aaf1faf0ca0c5326a338a0
michaelrauh <michaelrauh@ymail.com>
2024-04-07 11:08:24 -0400

    e2e example script. Could be more visible on failure

--- run_example.sh
+++ run_example.sh
@@ -1,1 +1,13 @@
+set -ex
+
+aws --endpoint-url http://192.168.1.9:9000 s3api create-bucket --bucket database
+
 cargo run -- --add example.txt --location database --endpoint http://192.168.1.9:9000
+aws --endpoint-url http://192.168.1.9:9000 s3 ls database/
+aws --endpoint-url http://192.168.1.9:9000 s3 cp s3://database/example.txt example-result.txt
+
+diff example.txt example-result.txt
+
+rm example-result.txt
+aws --endpoint-url http://192.168.1.9:9000 s3 rm s3://database/example.txt
+aws --endpoint-url http://192.168.1.9:9000 s3api delete-bucket --bucket database
